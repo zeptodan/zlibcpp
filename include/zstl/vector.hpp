7dfe3c7e56b693091b6d6c74784ad5bf61df4db8
Muhammad Anas <muhammadannas138@gmail.com>
2026-06-18 22:10:20 +0500

    extended test case for vector

--- include/zstl/vector.hpp
+++ include/zstl/vector.hpp
@@ -3,42 +3,48 @@ template<typename type>
 class vector{
     private:
     type* array;
-    type* size;
-    type* capacity;
+    type* size_;
+    type* capacity_;
     public:
     vector(){
         array = new type[10];
-        size = array;
-        capacity = array + 10;
+        size_ = array;
+        capacity_ = array + 10;
     }
     bool is_full(){
-         return (size == capacity) ? true : false;
+         return (size_ == capacity_) ? true : false;
     }
     void add(type& obj){
-        *size++ = obj;
+        *size_++ = obj;
         if(is_full()){
-            resize((capacity - array) * 1.5);
+            resize((capacity_ - array) * 1.5);
         }
     }
     void add(type&& obj){
-        *size++ = obj;
+        *size_++ = obj;
         if(is_full()){
-            resize((capacity - array) * 1.5);
+            resize((capacity_ - array) * 1.5);
         }
     }
     void resize(int new_size){
         type* temp = new type[new_size];
-        size = temp;
-        for (type* start = array; start != capacity;start++){
-            *size++ = *start;
+        size_ = temp;
+        for (type* start = array; start != capacity_;start++){
+            * size_ ++ = *start;
         }
         array = temp;
-        capacity = temp + new_size;
+        capacity_ = temp + new_size;
     }
     type& get(int index){
-        if(array + index >= capacity){
+        if(array + index >= capacity_){
             throw std::out_of_range("index out of bounds");
         }
         return array[index];
+    }
+    int size(){
+        return size_ - array;
+    }
+    int capacity(){
+        return capacity_ - array;
     }
 };
--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -4,4 +4,20 @@ TEST(vector_tests, vector_add){
     vector<int> vec;
     vec.add(4);
     ASSERT_EQ(vec.get(0),4);
+    int i = 5;
+    vec.add(i);
+    ASSERT_EQ(vec.get(1),5);
+    vec.add(4);
+    vec.add(4);
+    vec.add(4);
+    vec.add(4);
+    vec.add(4);
+    vec.add(4);
+    vec.add(4);
+    ASSERT_EQ(vec.is_full(),false);
+    ASSERT_EQ(vec.size(),9);
+    ASSERT_EQ(vec.capacity(),10);
+    vec.add(4);
+    ASSERT_EQ(vec.is_full(),false);
+
 }
