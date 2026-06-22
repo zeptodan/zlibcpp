752de05c94c31f7a93122444fd7fc23ea779e99b
zeptodan <muhammadannas138@gmail.com>
2026-06-22 19:25:11 +0500

    added decrement in vector iterator

--- include/zstl/vector_iterator.hpp
+++ include/zstl/vector_iterator.hpp
@@ -16,6 +16,18 @@ class vector_iterator{
         ptr_++;
         return temp;
     }
+    vector_iterator& operator--(){
+        --ptr_;
+        return *this;
+    }
+    vector_iterator operator--(int){
+        vector_iterator temp = *this;
+        ptr_--;
+        return temp;
+    }
+    int operator-(vector_iterator& itr){
+        return itr.ptr_ - ptr_;
+    }
     bool operator==(vector_iterator& itr){
         return ptr_ == itr.ptr_;
     }
