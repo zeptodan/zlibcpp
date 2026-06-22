3d0de009fc1d2448068c6f0fe678dd322d90b42c
zeptodan <muhammadannas138@gmail.com>
2026-06-22 15:12:52 +0500

    refactor to support profiling tools

--- benchmark/vector_benchmark.cpp
+++ benchmark/vector_benchmark.cpp
@@ -1,23 +1,46 @@
 #include<benchmark/benchmark.h>
 #include"zstl/vector.hpp"
+void zstl_vectoradd(vector<int>& vec,int n){
+    for(int i = 0; i < n; i++){
+        vec.add(i);
+    }
+}
+void stl_vectoradd(std::vector<int>& vec,int n){
+    for(int i = 0; i < n; i++){
+        vec.push_back(i);
+    }
+}
 static void BM_zstl_vectoradd(benchmark::State& state){
     for (auto _ : state){
         vector<int> vec;
-        for(int i = 0; i < state.range(0); i++){
-            vec.add(i);
-        }
+        zstl_vectoradd(vec,state.range(0));
         benchmark::DoNotOptimize(vec);
     }
 }
 static void BM_stl_vectoradd(benchmark::State& state){
     for (auto _ : state){
         std::vector<int> vec;
-        for(int i = 0; i < state.range(0); i++){
-            vec.push_back(i);
-        }
+        stl_vectoradd(vec, state.range(0));
         benchmark::DoNotOptimize(vec);
     }
 }
+int main(int argc, char* argv[]){
+    if (argc > 1){
+        std::string arg(argv[1]);
+        int n = 8 << 13;
+        if (arg == "cgA"){
+            vector<int> vec;
+            zstl_vectoradd(vec, n);
+            return 0;
+        }
+        else if(arg == "cgB"){
+            std::vector<int> vec;
+            stl_vectoradd(vec, n);
+            return 0;
+        }
+    }
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+}
 BENCHMARK(BM_zstl_vectoradd)->Range(8, 8 << 13);
 BENCHMARK(BM_stl_vectoradd)->Range(8, 8 << 13);
-BENCHMARK_MAIN();
