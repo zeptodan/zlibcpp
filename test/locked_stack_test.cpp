8b64608655714a8762526dbd52413cae908faf8a
Muhammad Anas <muhammadannas138@gmail.com>
2026-08-09 18:59:11 +0500

    added the parallel test for locked stack

--- test/locked_stack_test.cpp
+++ test/locked_stack_test.cpp
@@ -1,4 +1,6 @@
 #include<gtest/gtest.h>
+#include<array>
+#include<thread>
 #include"zstl/locked_stack.hpp"
 TEST(locked_stack_test, stack_add_pop){
     locked_stack<int> stack;
@@ -16,15 +18,37 @@ TEST(locked_stack_test, stack_add_try_pop){
     ASSERT_EQ(isval,true);
     ASSERT_EQ(val,5);
 }
-TEST(parallel_locked_stack_test, stack_add){
-
+TEST(parallel_locked_stack_test, stack_add_pop){
+    constexpr int tot_threads = 20;
+    constexpr int thread_pushes = 500;
+    std::atomic<int> tot_elems = tot_threads * thread_pushes;
+    std::array<std::thread, tot_threads> push_threads;
+    std::array<std::thread, tot_threads> pop_threads;
+    std::array<int, tot_threads> push_nums{};
+    std::array<std::atomic<int>, tot_threads> pop_nums{};
+    locked_stack<int> stack;
+    for (int i = 0; i < tot_threads;i++){
+        push_threads[i] = std::thread([i, &push_nums, &stack](){
+            while(push_nums[i]++ < 500){
+                stack.push(i);
+            }
+        });
+    }
+    for (int i = 0; i < tot_threads;i++){
+        pop_threads[i] = std::thread([&tot_elems, &pop_nums, &stack](){
+            while(tot_elems-- > 0){
+                pop_nums[*stack.pop()]++;
+            }
+        });
+    }
+    for (int i = 0; i < tot_threads;i++){
+        push_threads[i].join();
+        pop_threads[i].join();
+    }
+    for (int i = 0; i < tot_threads;i++){
+        ASSERT_EQ(pop_nums[i], thread_pushes) << "actual value is " << pop_nums[i] << "\n";
+    }
 }
-TEST(parallel_locked_stack_test, stack_pop){
-    
-}
-TEST(parallel_locked_stack_test, stack_try_pop){
-
-}
-TEST(parallel_locked_stack_test, even_stack_add_pop){
+TEST(parallel_locked_stack_test, stack_add_try_pop){
 
 }
