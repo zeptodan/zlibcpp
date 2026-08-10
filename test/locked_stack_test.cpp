34902a35bab51cb361fa2da4c052613445dafb97
Muhammad Anas <muhammadannas138@gmail.com>
2026-08-10 22:05:37 +0500

    added the parallel test cases for locked stack

--- test/locked_stack_test.cpp
+++ test/locked_stack_test.cpp
@@ -50,5 +50,38 @@ TEST(parallel_locked_stack_test, stack_add_pop){
     }
 }
 TEST(parallel_locked_stack_test, stack_add_try_pop){
-
+    constexpr int tot_threads = 20;
+    constexpr int thread_pushes = 500;
+    std::atomic<int> tot_elems = tot_threads * thread_pushes;
+    std::array<std::thread, tot_threads> push_threads;
+    std::array<std::thread, tot_threads> pop_threads;
+    std::array<int, tot_threads> push_nums{};
+    std::array<std::atomic<int>, tot_threads> pop_nums{};
+    locked_stack<int> stack;
+    for (int i = 0; i < tot_threads;i++){
+        push_threads[i] = std::thread([i, &push_nums, &stack](){
+            while(push_nums[i]++ < 500){
+                stack.push(i);
+            }
+        });
+    }
+    for (int i = 0; i < tot_threads;i++){
+        pop_threads[i] = std::thread([&tot_elems, &pop_nums, &stack](){
+            while(tot_elems > 0){
+                int val;
+                bool did_pop = stack.try_pop(val);
+                if (did_pop){
+                    --tot_elems;
+                    pop_nums[val]++;
+                }
+            }
+        });
+    }
+    for (int i = 0; i < tot_threads;i++){
+        push_threads[i].join();
+        pop_threads[i].join();
+    }
+    for (int i = 0; i < tot_threads;i++){
+        ASSERT_EQ(pop_nums[i], thread_pushes) << "actual value is " << pop_nums[i] << "\n";
+    }
 }
